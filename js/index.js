78ea94ec3aea604f2cdc5c36e686f46b8617c08c
Efficiency97 <Efficiency97@163.com>
2017-08-10 13:43:31 +0800

    header

---
+++ js/index.js
@@ -0,0 +1,25 @@
+
+$('.page_first').hover(
+	function () {
+		$('.page_border').css({'width':'85px','left':'630px'})
+	},
+	function () {
+		$('.page_border').css({'width':'0px','left':'0px'})
+	}
+);
+$('.page_type').hover(
+	function () {
+		$('.page_border').css({'width':'105px','left':'745px'})
+	},
+	function () {
+		$('.page_border').css({'width':'0px','left':'0px'})
+	}
+);
+$('.page_release').hover(
+	function () {
+		$('.page_border').css({'width':'105px','left':'893px'})
+	},
+	function () {
+		$('.page_border').css({'width':'0px','left':'0px'})
+	}
+)
