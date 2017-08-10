9ec9b1376fed8992077ecc8e21c6ee7663214fdb
Efficiency97 <Efficiency97@163.com>
2017-08-10 21:52:43 +0800

    header

--- js/index.js
+++ js/index.js
@@ -22,4 +22,14 @@ $('.page_release').hover(
 	function () {
 		$('.page_border').css({'width':'0px','left':'0px'})
 	}
-)
+);
+$('.page_typewrap').hover(
+	function () {
+		$('.type_menu').slideDown(400)
+	},
+	function () {
+		$('.type_menu').slideUp(100)
+	}
+);
+
+//登陆
