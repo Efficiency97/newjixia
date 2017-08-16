da4445ac83b74b3c7bd02bcc25b2c8e8232af455
Efficiency97 <Efficiency97@163.com>
2017-08-16 20:41:09 +0800

    publish

--- js/index.js
+++ js/index.js
@@ -15,14 +15,6 @@ $('.page_type').hover(
 		$('.page_border').css({'width':'0px','left':'0px'})
 	}
 );
-$('.page_release').hover(
-	function () {
-		$('.page_border').css({'width':'105px','left':'745px'})
-	},
-	function () {
-		$('.page_border').css({'width':'0px','left':'0px'})
-	}
-);
 $('.page_typewrap').hover(
 	function () {
 		$('.type_menu').slideDown(400)
