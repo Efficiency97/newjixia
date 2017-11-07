91546441d426e0b0968657997152aa1360e4917b
Efficiency97 <Efficiency97@163.com>
2017-11-07 17:11:18 +0800

    ji xia

--- js/index.js
+++ js/index.js
@@ -1,27 +1,13 @@
+$('.page_type').mouseenter(
+	function () {
+		$('.type_menu').show()
+	}
+);
+$('.type_menu').mouseleave(
+	function () {
+		$('.type_menu').hide()
+	}
 
-$('.page_first').hover(
-	function () {
-		$('.page_border').css({'width':'85px','left':'480px'})
-	},
-	function () {
-		$('.page_border').css({'width':'0px','left':'0px'})
-	}
-);
-$('.page_type').hover(
-	function () {
-		$('.page_border').css({'width':'105px','left':'595px'})
-	},
-	function () {
-		$('.page_border').css({'width':'0px','left':'0px'})
-	}
-);
-$('.page_typewrap').hover(
-	function () {
-		$('.type_menu').slideDown(400)
-	},
-	function () {
-		$('.type_menu').slideUp(100)
-	}
 );
 
 //登陆
